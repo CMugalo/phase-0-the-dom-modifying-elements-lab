85a3668e0ec72ce9a8fbc9d082048156b2f7951d
Christian Mugalo <christian.mugalo@student.moringaschool.com>
2024-04-07 18:38:39 +0300

    modifies DOM elements

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code here!
+ const mainClass = document.querySelector("#main")
+
+ const noClass = mainClass.remove()
+
+ const nodeName = document.createElement("h1")
+
+ const newHeader = nodeName
+
+ newHeader.id = "victory"
+
+ newHeader.textContent = "Christian Mugalo is the champion"
+
